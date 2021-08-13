89dceb2e3395857563f33fa4659821ae35dd6c9d
dhrutikaila <88370858+dhrutikaila@users.noreply.github.com>
2021-08-13 17:48:44 +0530

    Update DAY 4 RANK.sql

--- SQL/Dhruti/Day4/DAY 4 RANK.sql
+++ SQL/Dhruti/Day4/DAY 4 RANK.sql
@@ -1,38 +1,40 @@
 USE test1
 
 
---Q1
+--Q1 Write a query to rank employees based on their salary for a month
 SELECT FirstName+' '+LastName 'Name', Salary, RANK()
 OVER (ORDER BY Salary) 'Salary Ranking' FROM Employees
 
---Q2
+--Q2 Select 4th Highest salary from employee table using ranking function
 SELECT * FROM (
 SELECT FirstName+' '+LastName 'Name', Salary, Sal_Rank = RANK() 
 OVER (ORDER BY Salary DESC) FROM Employees
 ) temp
 WHERE Sal_Rank=4
+
+--Q3 Get department, total salary with respect to a department from employee table.
 SELECT DepartmentID, Total_Salary=SUM(Salary) from Employees
 GROUP BY DepartmentID
 
---Q4
+--Q4 Get department, total salary with respect to a department from employee table order by total salary descending
 
 SELECT DepartmentID, Total_Salary=SUM(Salary) from Employees
 GROUP BY DepartmentID
 ORDER BY Total_Salary DESC
 
---Q5
+--Q5 Get department wise maximum salary from employee table order by salary ascending
 
 SELECT DISTINCT DepartmentID,  dept_max_sal=(MAX(Salary) 
 OVER( PARTITION BY DepartmentID)) FROM Employees
 ORDER BY dept_max_sal ASC
 
---Q6
+--Q6 Get department wise minimum salary from employee table order by salary ascending
 
 SELECT DISTINCT DepartmentID, dept_min_sal=(MIN(Salary) 
 OVER( PARTITION BY DepartmentID)) FROM Employees
 ORDER BY dept_min_sal ASC
 
---Q7
+--Q7 Select department, total salary with respect to a department from employee table where total salary greater than 50000 order by TotalSalary descending
 SELECT * FROM 
 (
 SELECT DepartmentID, total_dept_sal=(SUM(Salary)) FROM Employees
